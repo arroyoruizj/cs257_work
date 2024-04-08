7da297088c409dc4c2d50eba777f4cb5adcacd73
arroyoruizj <arroyoruizj@carleton.edu>
2024-04-07 19:02:44 -0500

    Updated columns

--- sqllab/createtable.sql
+++ sqllab/createtable.sql
@@ -1,12 +1,14 @@
 DROP TABLE IF EXISTS earthquakes;
 CREATE TABLE earthquakes (
-  quakedate date,
-  quaketime time with time zone,
-  latitude real,
-  longitude real,
-  quakedepth real,
-  mag real,
-  id text,
-  place text,
-  quaketype text
+  time TIMESTAMPTZ,
+  latitude FLOAT(5, 2),
+  longitude FLOAT(5, 2),
+  depth FLOAT(5, 2),
+  magnitude FLOAT(3, 2),
+  dmin FLOAT(5, 2),
+  place STRING,
+  horizontalError FLOAT(5, 2),
+  depthError FLOAT(5, 2),
+  magError FLOAT(3, 2),
+  magNst INT(4)
 );

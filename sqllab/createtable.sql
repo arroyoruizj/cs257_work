5292e258193e54f5b238b8b39d94952f19a8c706
arroyoruizj <arroyoruizj@carleton.edu>
2024-04-07 19:06:43 -0500

    String error fixed

--- sqllab/createtable.sql
+++ sqllab/createtable.sql
@@ -6,7 +6,7 @@ CREATE TABLE earthquakes (
   depth NUMERIC(5, 2),
   magnitude NUMERIC(3, 2),
   dmin NUMERIC(5, 2),
-  place STRING,
+  place TEXT,
   horizontalError NUMERIC(5, 2),
   depthError NUMERIC(5, 2),
   magError NUMERIC(3, 2),

b992a113adb5cf2547977559f4c7b94947db9d2e
arroyoruizj <arroyoruizj@carleton.edu>
2024-04-10 10:38:41 -0500

    New Lab

---
+++ us-city-states/create_cities.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS cities;
+CREATE TABLE cities (
+city TEXT,
+state TEXT,
+population NUMERIC(8,0),
+latitude NUMERIC(5, 2),
+longitude NUMERIC(5, 2),
+);
---
+++ us-city-states/create_states.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXISTS states;
+CREATE TABLE states (
+code TEXT,
+state TEXT,
+pop NUMERIC(8,0)
+);

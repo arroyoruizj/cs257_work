09ba453bd6ac4ff31472cfc6ff220b15f196a64c
arroyoruizj <arroyoruizj@carleton.edu>
2024-04-15 14:23:36 -0500

    Making cityquery

---
+++ pythonlab/cityquery.py
@@ -0,0 +1,43 @@
+# We use the psycopg2 library to help us connec to the Postgres SQL database
+# This library is already installed on stearns.mathcs.carleton.edu
+import psycopg2
+
+# This function sends an SQL query to the database
+def test_query_one():
+
+    # You will need to change the Port and the Password to use this code
+    
+    conn = psycopg2.connect(
+        host="localhost",
+        port=5432,   
+        database="arroyoruizj",
+        user="arroyoruizj",
+        password="sunshine987chip")
+
+    cur = conn.cursor()
+
+    sql = "SELECT name, city FROM cities WHERE city = 'Northfield' "
+    
+    cur.execute( sql )
+
+    # fetchone() returns one row that matches your quer
+    row = cur.fetchone()
+
+    if row == None:
+
+        print("Northfield is not in the dateset")
+
+    else: 
+
+          return row
+
+    # Note: We could access individual items in the row
+    # That is, row[0] would be the name column in the previous example
+    #   ... and row[1] would be the abb column
+
+    #IMPORTANT: This function doesn't actually change the database
+    #If we are trying to change the database ...
+    # ... for example, creating a table
+    #Then we need the following command to finalize our changes
+
+    conn.commit()

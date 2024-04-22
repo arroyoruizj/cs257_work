c5b5f0135db0b155decb0005e4bb5488a35d3d5a
arroyoruizj <arroyoruizj@carleton.edu>
2024-04-22 10:40:02 -0500

    Population portion

--- flasklab/flask-test.py
+++ flasklab/flask-test.py
@@ -1,4 +1,5 @@
 import flask
+import psycopg2
 
 app = flask.Flask(__name__)
 
@@ -9,7 +10,7 @@ def my_function():
 
 @app.route('/display/<word1>/<word2>')
 def my_display(word1, word2):
-    the_string = "The words are: " + word1 + " and " + word2;
+    the_string = "The words are: " + word1 + " and " + word2
     return the_string
 
 @app.route('/color/<word1>')
@@ -20,6 +21,26 @@ def my_color(word1):
 def addition(num1, num2):
     sum = str(int(num1) + int(num2))
     return "The sum of your numbers is: " + sum
+
+@app.route('/pop/<abbrev>')
+def pop(abbrev):
+
+    conn = psycopg2.connect(
+        host="localhost",
+        port=5432,   
+        database="arroyoruizj",
+        user="arroyoruizj",
+        password="sunshine987chip")
+
+    cur = conn.cursor()
+
+    sql = "SELECT pop, code FROM states WHERE code = %s'"
+    
+    cur.execute(sql, [abbrev])
+    
+    row = cur.fetchone()
+
+    return row
 
 if __name__ == '__main__':
     my_port = 5112

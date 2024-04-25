396ab6f91232ee82c12e9ca84f93d65263d93d19
arroyoruizj <arroyoruizj@carleton.edu>
2024-04-24 20:32:48 -0500

    Starter Code

--- flasklab/rand_sentence.py
+++ flasklab/rand_sentence.py
@@ -7,3 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def welcome():
     return render_template("starter.html")
+
+if __name__ == '__main__':
+    my_port = 5112
+    app.run(host='0.0.0.0', port = my_port) 

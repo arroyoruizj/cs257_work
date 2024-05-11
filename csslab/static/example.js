496fc70327b57b0182a9ed3aca2be32d57516a53
arroyoruizj <arroyoruizj@carleton.edu>
2024-05-11 18:58:12 -0500

    Editing images

--- csslab/static/example.js
+++ csslab/static/example.js
@@ -3,3 +3,16 @@ function changeColor() {
   the_heading.style.color = "red";
   console.log("I just changed the color to: " + the_heading.style.color)  
 }
+
+function reveal(clicked_image) {
+  image_clicked = document.getElementById(clicked_image)
+  random_num = randint(1, 3)
+
+  if (random_num == 3){
+    image_clicked.src = "static/images/money_door.png"
+  }
+  else {
+    image_clicked.src = "static/image/snake_door.png"
+  }
+  
+}
